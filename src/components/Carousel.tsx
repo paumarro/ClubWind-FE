b49eca62b38d9271473350ca5848aa80ed25e32a
paumarro <pau@peaksandpies.com>
2024-03-04 16:30:03 +0100

    refactor EventCarousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { AddEvent } from "./AddEvent";
 import { EventBox } from "./EventBox";
 import { events }  from '../testData'
+import { CarouselController } from "./CarouselController";
 
 export const Carousel = (props) => {
 
@@ -27,6 +28,9 @@ export const Carousel = (props) => {
   return (
     <>
       <div className="w-full flex-col mb-10">
+        
+      <CarouselController handleStart={handleStart} handleNext={handleNext} handlePrevious={handlePrevious} />
+
         <div className="relative w-full flex">
           <div className="w-1/5 h-full">
             <div id="eventsCarousel" className="snap-normal flex relative">
@@ -35,7 +39,7 @@ export const Carousel = (props) => {
                       <AddEvent />  
               {events.map((event, index) => (
                       
-                      <EventBox title={event.title} date={event.date} description={event.description} />
+                      <EventBox title={event.title} date={event.date} description={event.description} img={event.img} />
                 
               ))}
               </div>
@@ -44,69 +48,7 @@ export const Carousel = (props) => {
         </div>
       </div>
 
-
-
-      <div className="absolute right-12 top-60 flex">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="gray"
-          className=" w-12 h-12 px-3 mr-1  hover:fill-gray-700 "
-        >
-          <path d="M18.75 12.75h1.5a.75.75 0 000-1.5h-1.5a.75.75 0 000 1.5zM12 6a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 6zM12 18a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 18zM3.75 6.75h1.5a.75.75 0 100-1.5h-1.5a.75.75 0 000 1.5zM5.25 18.75h-1.5a.75.75 0 010-1.5h1.5a.75.75 0 010 1.5zM3 12a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 013 12zM9 3.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5zM12.75 12a2.25 2.25 0 114.5 0 2.25 2.25 0 01-4.5 0zM9 15.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z" />
-        </svg>
-
-        <button className="mx-2" onClick={handleStart}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-10 h-10 bg-white rounded-full drop-shadow px-3 hover:bg-gray-100"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-        </button>
-
-        <button className="mx-2" onClick={handlePrevious}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-10 h-10 bg-white rounded-full drop-shadow px-3 hover:bg-gray-100"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M15.75 19.5L8.25 12l7.5-7.5"
-            />
-          </svg>
-        </button>
-
-        <button className="mx-2" onClick={handleNext}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-10 h-10 bg-white rounded-full drop-shadow px-3 hover:bg-gray-100"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M8.25 4.5l7.5 7.5-7.5 7.5"
-            />
-          </svg>
-        </button>
-      </div>
-    </>
+      
+    </>  
   );
 };
---
+++ src/components/CarouselController.tsx
@@ -0,0 +1,65 @@
+export const CarouselController = ({handleStart, handlePrevious, handleNext}) => {
+    return(
+        <div className="carousel-controller mb-3 float-right mr-8 flex r-100 z-40">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-10 h-10 bg-gray/10 rounded-full px-3 hover:bg-gray-300 opacity-40 duration-300"
+        >
+          <path d="M18.75 12.75h1.5a.75.75 0 000-1.5h-1.5a.75.75 0 000 1.5zM12 6a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 6zM12 18a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 18zM3.75 6.75h1.5a.75.75 0 100-1.5h-1.5a.75.75 0 000 1.5zM5.25 18.75h-1.5a.75.75 0 010-1.5h1.5a.75.75 0 010 1.5zM3 12a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 013 12zM9 3.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5zM12.75 12a2.25 2.25 0 114.5 0 2.25 2.25 0 01-4.5 0zM9 15.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z" />
+        </svg>
+
+        <button className="mx-2" onClick={handleStart}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-10 h-10 bg-gray/10 rounded-full px-3 hover:bg-gray-300 opacity-40 duration-300"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </button>
+
+        <button className="mx-2" onClick={handlePrevious}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-10 h-10 bg-gray/10 rounded-full px-3 hover:bg-gray-300 opacity-40 duration-300"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 19.5L8.25 12l7.5-7.5"
+            />
+          </svg>
+        </button>
+
+        <button className="mx-2" onClick={handleNext}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-10 h-10 bg-gray/10 rounded-full px-3 hover:bg-gray-300 opacity-40 duration-300"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </button>
+      </div>
+    )
+}
