11171a0cb0fa1672268c6f2a69c2af119775ad34
paumarro <pau@peaksandpies.com>
2024-05-27 11:32:26 +0200

    resize header image

--- src/components/HeaderIMG.tsx
+++ src/components/HeaderIMG.tsx
@@ -2,7 +2,7 @@ import logo from '../imgs/CW_Logo.png'
 
 const HeaderIMG = () => {
   return <a href="http://localhost:3001/" >
-  <img className='object-scale-down h-7 mt-3' src={logo} alt='logo' />
+  <img className='object-scale-down h-5 mt-3' src={logo} alt='logo' />
           </ a>
         
   
